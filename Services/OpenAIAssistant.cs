90ddfd507703510664d6b980929fffc474d42f5a
chungkai1029 <chungkai1029@gmail.com>
2024-05-02 23:02:47 +0800

    Feature: spam message detect robot

---
+++ Services/OpenAIAssistant.cs
@@ -0,0 +1,67 @@
+using System.Text;
+using System.Text.Json;
+using Azure.AI.OpenAI;
+using BECamp_T13_HW2_Aspnet_AI.Data;
+
+namespace BECamp_T13_HW2_Aspnet_AI.Services
+{
+    internal class OpenAIAssistant
+    {
+        // To get the OpenAI API key that is stored in the user-secrets list.
+        static IConfigurationRoot userSecretConfig = new ConfigurationBuilder()
+                .AddUserSecrets<OpenAIAssistant>()
+                .Build();
+
+        static string nonAzureOpenAIApiKey = userSecretConfig["OpenAI:APIKey"];
+
+        // The stream chat module using in text generation.
+        async Task<string> StreamChatWithNonAzureOpenAI(OpenAIClient client, string prompt)
+        {
+            // The configuration information for a chat completions request.
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                DeploymentName = "gpt-3.5-turbo",
+                Messages =
+                {
+                    new ChatRequestUserMessage(prompt)
+                }
+            };
+
+            // Build a response string.
+            StringBuilder response = new StringBuilder();
+            // Represent an incremental update to a streamed chat response.
+            await foreach (StreamingChatCompletionsUpdate chatUpdate in client.GetChatCompletionsStreaming(chatCompletionsOptions))
+            {
+                // Composite the response from the OpenAI.
+                {
+                    response.Append(chatUpdate.ContentUpdate);
+                }
+            }
+
+            return response.ToString();
+        }
+
+        internal async Task<string> SpamCheck(string prompt)
+        {
+            OpenAIClient spamClient = new OpenAIClient(nonAzureOpenAIApiKey, new OpenAIClientOptions());
+            // Build the prompt string to ask AI assistant.
+            StringBuilder spamPrompt = new StringBuilder("You are a forum moderator who always responds using JSON.");
+            spamPrompt.Append("Please inspect the following text and determine if it is spam.");
+            spamPrompt.Append(new { prompt }.ToString());
+            spamPrompt.Append("Expected Response Example: {\"is_spam\": true|false}");
+
+            string streamChatResponse = await StreamChatWithNonAzureOpenAI(spamClient, spamPrompt.ToString());
+            // Parses the text to a single JSON value.
+            IsSpam isSpam = JsonSerializer.Deserialize<IsSpam>(streamChatResponse);
+
+            if (isSpam.is_spam == true)
+            {
+                return "Spam was detected.";
+            }
+            else
+            {
+                return "Post is valid.";
+            }
+        }
+    }
+}
