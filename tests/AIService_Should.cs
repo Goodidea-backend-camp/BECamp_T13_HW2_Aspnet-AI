2557945258b755440ac9907dc4cff701c273db5b
chungkai1029 <chungkai1029@gmail.com>
2024-06-26 15:46:31 +0800

    test: add a simple test of spam check with xUnit

---
+++ tests/AIService_Should.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using BECamp_T13_HW2_Aspnet_AI.Services;
+
+namespace BECamp_T13_HW2_Aspnet_AI
+{
+    public class AIService_Should
+    {
+        private string postIsValid = "Spam was detected.";
+        private string spamWasDetect = "Post is valid.";
+
+        [Fact]
+        public async void SpamCheck_IsSpamDetect()
+        {
+            IAIServices services = new OpenAIServices();
+
+            string fuckYouResult = await services.SpamCheck("Fuck You!");
+            string helloWorldResult = await services.SpamCheck("Hello, world!");
+
+            Assert.Equal(spamWasDetect, fuckYouResult);
+            Assert.Equal(postIsValid, helloWorldResult);
+        }
+    }
+}
